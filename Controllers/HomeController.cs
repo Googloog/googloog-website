96680941b2cf0e37c4739e20d32255667126a9cb
CheckerPhil <91223795+CheckerPhil@users.noreply.github.com>
2022-04-18 14:30:49 +0200

    Added Links to Home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Sooziales_Netzwerk.Models;
+using Microsoft.EntityFrameworkCore;
+using Sooziales_Netzwerk.Data;
 
 namespace Sooziales_Netzwerk.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly Sooziales_NetzwerkDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    /*public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+    }*/
+
+    public HomeController(Sooziales_NetzwerkDbContext context)
+    {
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return View(await _context.Link.ToListAsync());
     }
 
     public IActionResult Privacy()
